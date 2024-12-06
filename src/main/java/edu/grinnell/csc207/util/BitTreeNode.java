a9b66c056053f3ad66262601c63c92766a9e041b
linricha <linricha@grinnell.edu>
2024-12-05 19:08:06 -0600

    Added comments

--- src/main/java/edu/grinnell/csc207/util/BitTreeNode.java
+++ src/main/java/edu/grinnell/csc207/util/BitTreeNode.java
@@ -1,22 +1,39 @@
 package edu.grinnell.csc207.util;
 
+/**
+ * A node used for creating / implementing BitTree.
+ *
+ * @author Richard Lin
+ */
 public class BitTreeNode {
   // +--------+------------------------------------------------------
   // | Fields |
   // +--------+
 
+  /**
+   * The left child node of BitTreeNode.
+   */
   BitTreeNode leftChild;
 
+  /**
+   * The right child node of BitTreeNode.
+   */
   BitTreeNode rightChild;
 
+  /**
+   * The value stored in the node.
+   */
   String value;
 
   // +--------------+------------------------------------------------
   // | Constructors |
   // +--------------+
 
+  /**
+   * Constructs the BitTreeNode Object.
+   */
   public BitTreeNode () {
-  }
+  } // BitTreeNode()
 
   // +---------+-----------------------------------------------------
   // | Methods |
